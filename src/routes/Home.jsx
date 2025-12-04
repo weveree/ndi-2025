9a28979877578bea5c09c9a74799078c5602ae47
Clément Songis <clement.songis@etu.univ-grenoble-alpes.fr>
2025-12-04 19:23:00 +0100

    Remove shit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,7 @@
-import reactLogo from '@/assets/react.svg';
-import Counter from '@/components/Counter';
 import styles from './Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.app}>
-      <img src={reactLogo} alt="React Logo" />
-      <Counter />
-    </div>
-  );
+  return <div className={styles.app}>
+    
+  </div>;
 }
