8448b875b4238221f3c74bbca48e64134ca9eec4
Clément Songis <clement.songis@etu.univ-grenoble-alpes.fr>
2025-12-05 03:29:54 +0100

    Slow alerts

--- src/components/main-chalenge/GameManager.jsx
+++ src/components/main-chalenge/GameManager.jsx
@@ -5,7 +5,7 @@ export const GameManagerContext = createContext();
 
 export default function GameManager({ children }) {
   const [data, setData] = useState({
-    money: 0,
+    money: 1000000,
     energy: 0,
   });
 
@@ -27,32 +27,43 @@ export default function GameManager({ children }) {
   }
 
   function changeMoney(money) {
-    changeAttribute('money', (prev) => prev - money);
+    changeAttribute('money', (prev) => prev + money);
   }
   function changeEnergy(energy) {
-    changeAttribute('energy', (prev) => prev - energy);
+    changeAttribute('energy', (prev) => prev + energy);
   }
 
   function addAlert(alert) {
     setAlerts((prev) => [...prev, { id: crypto.randomUUID(), ...alert }]);
   }
   function removeAlert(alert) {
-    setAlerts((prev) => prev.filter(el=>el.id!== alert.id));
+    setAlerts((prev) => prev.filter((el) => el.id !== alert.id));
   }
 
   function loop() {
-    changeMoney(50);
+    changeMoney(-1);
 
     console.log(data.money);
     const alertTypes = Object.keys(ALERTS);
-    addAlert({
-      type: alertTypes[Math.floor(Math.random() * alertTypes.length)],
-      ...ALERT_POSITIONS[Math.floor(Math.random() * ALERT_POSITIONS.length)],
-    });
+
+    if (Math.random() < 0.2) {
+      const position = ALERT_POSITIONS[Math.floor(Math.random() * ALERT_POSITIONS.length)];
+
+      if (alerts.find((a) => a.x === position.x && a.y === position.y)) {
+        return;
+      }
+
+      addAlert({
+        type: alertTypes[Math.floor(Math.random() * alertTypes.length)],
+        ...position,
+      });
+    }
   }
 
   return (
-    <GameManagerContext.Provider value={{ data, alerts, currentModal, setCurrentModal,changeMoney,changeEnergy,removeAlert }}>
+    <GameManagerContext.Provider
+      value={{ data, alerts, currentModal, setCurrentModal, changeMoney, changeEnergy, removeAlert }}
+    >
       {children}
     </GameManagerContext.Provider>
   );
