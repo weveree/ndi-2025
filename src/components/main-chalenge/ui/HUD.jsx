71b36bdd26d396d565362e1722bc9267489ee191
Clément Songis <clement.songis@etu.univ-grenoble-alpes.fr>
2025-12-05 01:52:05 +0100

    Use real values

--- src/components/main-chalenge/ui/HUD.jsx
+++ src/components/main-chalenge/ui/HUD.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { FaMoneyBill } from 'react-icons/fa'
-import { MdBolt, MdMoney } from 'react-icons/md'
+import React from 'react';
+import { FaMoneyBill } from 'react-icons/fa';
+import { MdBolt } from 'react-icons/md';
+import { useGameManager } from '../GameManager';
 
 export default function HUD() {
+  const { data } = useGameManager();
   return (
-    <section className='absolute z-50 left-0 bottom-0 h-fit bg-white w-90 rounded-tr-2xl p-5 *:flex *:items-center'>
-        <div>
-            <FaMoneyBill className='text-green-500 text-2xl'/> <p className='font-bold text-2xl'>&nbsp;28937 $</p>
-        </div>
-        <div>
-            <MdBolt className='text-yellow-400 text-2xl'/> <p className='font-bold text-2xl'>100%</p>
-        </div>
-        
+    <section className="absolute z-50 left-0 bottom-0 h-fit bg-white w-90 rounded-tr-2xl p-5 *:flex *:items-center">
+      <div>
+        <FaMoneyBill className="text-green-500 text-2xl" /> <p className="font-bold text-2xl">&nbsp;{data.money} $</p>
+      </div>
+      <div>
+        <MdBolt className="text-yellow-400 text-2xl" /> <p className="font-bold text-2xl">{data.energy}%</p>
+      </div>
     </section>
-  )
+  );
 }
